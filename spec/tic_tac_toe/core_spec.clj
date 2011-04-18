f269e97dd43c3c2c42f1d61d095a6d7cb932effd
calebphillips <caleb.phillips@gmail.com>
2011-04-18 10:07:23 -0500

    Detects vertical wins

--- spec/tic_tac_toe/core_spec.clj
+++ spec/tic_tac_toe/core_spec.clj
@@ -4,27 +4,61 @@
 
 (describe "Winning"
           (it "detects horizontal wins"
-              (should= :x (winner [[:x :x :x] 
-                                   [:o :x :o] 
-                                   [:o :o :x]]))
+              (should= :x (winner [:x :x :x,
+                                   :o :x :o,
+                                   :o :o :x]))
 
-              (should= :x (winner [[:o :x :o] 
-                                   [:x :x :x]
-                                   [:o :o :x]]))
+              (should= :x (winner [:o :x :o,
+                                   :x :x :x,
+                                   :o :o :x]))
 
-              (should= :x (winner [[:o :x :o] 
-                                   [:o :o :x]
-                                   [:x :x :x]]))
+              (should= :x (winner [:o :x :o,
+                                   :o :o :x,
+                                   :x :x :x]))
 
-              (should= :o (winner [[:o :o :o] 
-                                   [:x :o :x] 
-                                   [:x :x :o]]))
+              (should= :o (winner [:o :o :o,
+                                   :x :o :x,
+                                   :x :x :o]))
 
-              (should= :o (winner [[:x :o :x] 
-                                   [:o :o :o]
-                                   [:x :x :o]]))
+              (should= :o (winner [:x :o :x,
+                                   :o :o :o,
+                                   :x :x :o]))
 
-              (should= :o (winner [[:x :o :x] 
-                                   [:x :x :o]
-                                   [:o :o :o]]))
-              ))
+              (should= :o (winner [:x :o :x,
+                                   :x :x :o,
+                                   :o :o :o]))
+              )
+          
+          (it "detects vertical wins"
+              (should= :x (winner [:x :o :x,
+                                   :x :o :o,
+                                   :x :x :o]))
+
+              (should= :x (winner [:o :x :o,
+                                   :x :x :o,
+                                   :o :x :x]))
+
+              (should= :x (winner [:o :x :x,
+                                   :o :o :x,
+                                   :x :o :x]))
+
+              (should= :o (winner [:o :x :o,
+                                   :o :x :x,
+                                   :o :o :x]))
+
+              (should= :o (winner [:x :o :x,
+                                   :o :o :x,
+                                   :x :o :o]))
+
+              (should= :o (winner [:x :o :o,
+                                   :x :x :o,
+                                   :o :x :o]))
+              )
+
+          ; (it "detects diagonal wins"
+              ; (should= :x (winner [:x :o :o
+                                   ; :o :x :o
+                                   ; :o :o :x]))
+              ; )
+
+          )
--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,8 +1,26 @@
 (ns tic-tac-toe.core)
 
-(defn winner [board]
-  (second
+(defn rows [board]
+  (partition 3 board))
+
+(defn columns [board]
+  (for [i (range 3)] (map #(nth % i) (partition 3 board))))
+
+
+(defn three-in-a-row [colls]
+  (first 
     (first
-      (filter #(= 3 (first %)) 
-              (map (juxt count first) 
-                   (apply concat (map (partial partition-by identity) board)))))))
+      (filter #(= 3 (count %))
+              (apply concat (map (partial partition-by identity) colls))))))
+
+(defn horizontal-winner [board]
+  (three-in-a-row (rows board)))
+
+(defn vertical-winner [board]
+  (three-in-a-row (columns board)))
+
+(defn winner [board]
+  "Returns the keyword for the winner, if any, otherwise returns nil"
+  (let [hw (horizontal-winner board)
+        vw (vertical-winner board)]
+    (if hw hw vw)))
