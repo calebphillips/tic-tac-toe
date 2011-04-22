0382ba01a6f2b0d0cc2b16d5ea0e8f1dba019e30
calebphillips <caleb.phillips@gmail.com>
2011-04-21 23:59:25 -0500

    A little cosmetic clean up

--- TODO.md
+++ TODO.md
@@ -1,11 +1,6 @@
-* create a utility function to generate the complete set of board states and hammer the player with them
-* coverage?
-* main function
-* get rid of colons in keywords
-* display numbers for open spaces
-* check for bad input
 * standardize names (pos/position)
+* review tests for formatting, names, etc.
 * break up namespaces (game?)
-* figure out how to test the game
+* make sure game coverage is sufficient
 * End the game when no one can win (?)
-* update computer logic to try to get squeezes, before going to the middle
+* create a utility function to generate the complete set of board states and hammer the player with them
--- src/tic_tac_toe/game.clj
+++ src/tic_tac_toe/game.clj
@@ -1,16 +1,18 @@
 (ns tic-tac-toe.game
-    (:gen-class)
-    (:use [tic-tac-toe.core]
-          [clojure.string :only [join]]))
+  (:gen-class)
+  (:use [tic-tac-toe.core]
+        [clojure.string :only [join]]))
+
+(defn format-space [value]
+  (if (nil? value)
+    "   "  
+    (str " " (.toUpperCase (name value)) " "))) 
 
 (defn format-board [board]
   (do 
     (println)
-    (doseq [r (rows (indexed-board board))] 
-           (println (interpose "|" 
-                               (map #(if (nil? (second %)) 
-                                       (str "   ")  
-                                       (str " " (.toUpperCase (name (second %))) " ")) r))))
+    (doseq [r (rows board)] 
+      (println (interpose "|" (map format-space r))))
     (println)))
 
 (defn- init-board []
@@ -18,42 +20,42 @@
 
 
 (defn pr-msg [board msg]
-      (let [banner (apply str (repeat 40 "*"))] 
-        (format-board board)
-        (println)
-        (println banner)
-        (println (str "    " msg))
-        (println banner)
-        (println)))
+  (let [banner (apply str (repeat 40 "*"))] 
+    (format-board board)
+    (println)
+    (println banner)
+    (println (str "    " msg))
+    (println banner)
+    (println)))
 
 (defn pr-victory-msg [board the-winner]
-      (pr-msg board (str (.toUpperCase (name the-winner)) " has won the game!")))
+  (pr-msg board (str (.toUpperCase (name the-winner)) " has won the game!")))
 
 (defn pr-tie-msg [board]
-      (pr-msg board "The game has ended in a tie."))
+  (pr-msg board "The game has ended in a tie."))
 
 (defn prompt [board]
-      (do
-        (format-board board)
-        (print (str "Please select a move (" (join "," (empty-cells board)) "): "))
-        (flush))) 
+  (do
+    (format-board board)
+    (print (str "Please select a move (" (join "," (empty-cells board)) "): "))
+    (flush))) 
 
 ; Really in need of love
 (defn read-one-move [board]
-      (try 
-        (let [move (Integer. (read-line))] 
-          (when (< -1 move 9) 
-            (when (some #{move} (empty-cells board)) move)))
-        (catch NumberFormatException _ nil)))
+  (try 
+    (let [move (Integer. (read-line))] 
+      (when (< -1 move 9) 
+        (when (some #{move} (empty-cells board)) move)))
+    (catch NumberFormatException _ nil)))
 
 (defn read-move [board]
-      (loop [move (read-one-move board)]
-            (if move
-              move
-              (do
-                (print (str "Invalid move, please select another move: "))
-                (flush)
-                (recur (read-one-move board))))))
+  (loop [move (read-one-move board)]
+    (if move
+      move
+      (do
+        (print (str "Invalid move, please select another move: "))
+        (flush)
+        (recur (read-one-move board))))))
 
 (defn game []
   (loop [board (init-board) the-winner nil]
@@ -70,13 +72,13 @@
                 (recur board (winner board))))))))))
 
 (defn welcome []
-     (let [banner (apply str (repeat 50 "="))]
-       (println banner)
-       (println)
-       (println "        Tic Tac Toe")
-       (println)
-       (println banner)))
+  (let [banner (apply str (repeat 50 "="))]
+    (println banner)
+    (println)
+    (println "        Tic Tac Toe")
+    (println)
+    (println banner)))
 
 (defn -main [& args]
-      (do (welcome) (game)))
+  (do (welcome) (game)))
 
