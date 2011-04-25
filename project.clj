b8f5a3b2e18722ed3810c35192149a40a3af36b6
calebphillips <caleb.phillips@gmail.com>
2011-04-24 23:00:55 -0500

    Updated project version number

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject tic-tac-toe "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject tic-tac-toe "1.0.0"
+  :description "Text interfaced Tic Tac Toe game"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[speclj "1.2.0"]]
